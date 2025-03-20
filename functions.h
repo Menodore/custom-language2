62bce3cda48a1af870c54e06fc467778dd855753
Ananya Srivastava <120155874+Menodore@users.noreply.github.com>
2025-03-20 09:03:51 +0000

    new

---
+++ functions.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void next();
+void match(int tk);
+void expression(int level);
+void statement();
+void enum_declaration();
+void function_parameter() ;
+void function_body();
+void function_declaration();
+void global_declaration();
+void program();
+int eval();
+
+#endif // FUNCTIONS_H
