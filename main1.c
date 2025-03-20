c78a1ce1b86db7a57a8c33f28a8e1e7f4cd95ab1
Ananya Srivastava <120155874+Menodore@users.noreply.github.com>
2025-03-20 09:35:39 +0000

    new

--- main1.c
+++ main1.c
@@ -1,119 +1,125 @@
-#include "globals.h"
-#include "functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "global.h"
+#include "tokens.h"
+#include "parser.h"
+// #include "vm.h"
 
-int main(int argc, char **argv)
+// void expression()
+// {
+//   return;
+// }
+
+
+void initialise(int argc, char const *argv[])
 {
-    #define int long long // to work with 64bit address
+  int i,fd;
 
-    int i, fd;
-    int *tmp;
+  argc--;
+  argv++;
+  poolsize=1024;
+  scope = &symtab;
 
-    argc--;
-    argv++;
+  if (argc == 1)
+  {
+    SOURCE = 1;
+  }
 
-    // parse arguments
-    if (argc > 0 && **argv == '-' && (*argv)[1] == 's') {
-        assembly = 1;
-        --argc;
-        ++argv;
+  if (SOURCE)
+  {
+    if ((fd=open(*argv,0))<0)
+    {
+      die("open");
     }
-    if (argc > 0 && **argv == '-' && (*argv)[1] == 'd') {
-        debug = 1;
-        --argc;
-        ++argv;
-    }
-    if (argc < 1) {
-        printf("usage: xc [-s] [-d] file ...\n");
-        return -1;
-    }
-
-    if ((fd = open(*argv, 0)) < 0) {
-        printf("could not open(%s)\n", *argv);
-        return -1;
+    if ((old_src=src=(char*)malloc(poolsize))<0)
+    {
+      die("malloc");
     }
 
-    poolsize = 256 * 1024; // arbitrary size
-    line = 1;
-
-    // allocate memory
-    if (!(text = malloc(poolsize))) {
-        printf("could not malloc(%lld) for text area\n", poolsize);
-        return -1;
-    }
-    if (!(data = malloc(poolsize))) {
-        printf("could not malloc(%lld) for data area\n", poolsize);
-        return -1;
-    }
-    if (!(stack = malloc(poolsize))) {
-        printf("could not malloc(%lld) for stack area\n", poolsize);
-        return -1;
-    }
-    if (!(symbols = malloc(poolsize))) {
-        printf("could not malloc(%lld) for symbol table\n", poolsize);
-        return -1;
+    if ((i=read(fd,src,poolsize-1))<0)
+    {
+      die("read");
     }
 
-    memset(text, 0, poolsize);
-    memset(data, 0, poolsize);
-    memset(stack, 0, poolsize);
-    memset(symbols, 0, poolsize);
-
-    old_text = text;
-
-    src = "char else enum if int return sizeof while "
-          "open read close printf malloc memset memcmp exit void main";
-
-     // add keywords to symbol table
-    i = Char;
-    while (i <= While) {
-        next();
-        current_id[Token] = i++;
+    if (!(old_text=text=(long*)malloc(poolsize)))
+    {
+      die("malloc");
     }
+  }
 
-    // add library to symbol table
-    i = OPEN;
-    while (i <= EXIT) {
-        next();
-        current_id[Class] = Sys;
-        current_id[Type] = INT;
-        current_id[Value] = i++;
-    }
+  if (!(stack=(val*)malloc(poolsize)))
+  {
+    die("malloc");
+  }
 
-    next(); current_id[Token] = Char; // handle void type
-    next(); idmain = current_id; // keep track of main
+  if (!(call_stack=(long*)malloc(poolsize)))
+  {
+    die("malloc");
+  }
 
-    if (!(src = old_src = malloc(poolsize))) {
-        printf("could not malloc(%lld) for source area\n", poolsize);
-        return -1;
-    }
-    // read the source file
-    if ((i = read(fd, src, poolsize-1)) <= 0) {
-        printf("read() returned %lld\n", i);
-        return -1;
-    }
-    src[i] = 0; // add EOF character
+  if (!(data=(char*)malloc(poolsize)))
+  {
+    die("malloc");
+  }
+
+  memset(stack,0,poolsize);
+  memset(call_stack,0,poolsize);
+  memset(data,0,poolsize);
+
+  if (SOURCE)
+  {
+    memset(text,0,poolsize);
+    orig=text;
+    text=text-1;
+    src[i]='\0';
     close(fd);
+  }
 
-    program();
+  sp = bp = (val*)((long)stack+poolsize);
+  cs_sp = cs_bp = (long*)((long)call_stack+poolsize);
+  ax.type = 0;
+  ax.data = 0;
+  pc = 0;
 
-    if (!(pc = (int *)idmain[Value])) {
-        printf("main() not defined\n");
-        return -1;
-    }
+  char* tmp = src;
+  src = "func else enum if return sizeof while exit open read close printf malloc memset memcmp\0";
+  i = Func;
+  while (i<=While)
+  {
+    next();
+    cur->tok = i;
+    i++;
+  }
+  // i=EXIT;
+  // while (i<=MCMP)
+  // {
+  //   next();
+  //   cur->class = Sys;
+  //   cur->value = i++;
+  //   // cur->type = Int;
+  // }
+  src=tmp;
 
-    // dump_text();
-    if (assembly) {
-        // only for compile
-        return 0;
-    }
+  setvbuf(stdout, NULL, _IONBF, 0);
+  setvbuf(stdin, NULL, _IONBF, 0);
+  setvbuf(stderr, NULL, _IONBF, 0);
 
-    // setup stack
-    stk = (int *)((int)stack + poolsize);
-    *--stk = EXIT; // call exit if main returns
-    *--stk = PUSH; tmp = stk;
-    *--stk = argc;
-    *--stk = (int)argv;
-    *--stk = (int)tmp;
+}
 
-    return eval();
+int main(int argc, char const *argv[])
+{
+
+  initialise(argc,argv);
+  // puts(src);
+  program();
+  // emit(EXIT);
+  // puts("");
+  // eval();
+
+  return 0;
 }
